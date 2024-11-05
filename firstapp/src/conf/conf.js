f01e6b63828134d88481f882ec2795f28517724b
Imagekafle10 <hpimage10@gmail.com>
2024-11-05 08:10:18 +0545

    first commit
    sample Deployment

---
+++ firstapp/src/conf/conf.js
@@ -0,0 +1,12 @@
+const conf={
+    appwriteUrl: String(process.env.REACT_APP_APPWRITE_URL),
+    
+    appwriteProjectId: String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(process.env.REACT_APP_APPWRITE_BUCKET_ID),
+}
+// console.log(String(process.env.REACT_APP_APPWRITE_URL));
+
+
+export default conf
